56969c9773529bf79eab71247a8bab50f577961f
sirpolux <poluxreloaded@gmail.com>
2023-09-03 23:53:22 +0100

    feat-Created boilerplate

---
+++ spring_security_v11_demo/src/main/java/com/example/spring_security_v11_demo/SpringSecurityV11DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_v11_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityV11DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityV11DemoApplication.class, args);
+	}
+
+}
