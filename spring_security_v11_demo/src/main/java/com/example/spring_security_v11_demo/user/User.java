925f729a71831f8554a0f375086ba8c14be1055f
sirpolux <poluxreloaded@gmail.com>
2023-09-03 23:54:34 +0100

    feat-Created User class(Entity)

---
+++ spring_security_v11_demo/src/main/java/com/example/spring_security_v11_demo/user/User.java
@@ -0,0 +1,12 @@
+package com.example.spring_security_v11_demo.user;
+
+
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String role;
+    private boolean isEnabled=false;
+}
